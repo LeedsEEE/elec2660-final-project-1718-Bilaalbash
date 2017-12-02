1b8034b72c2dc90311b744084583514ccada8ec1
Bilaal Bashir <Billy@Bilaals-MacBook-Pro.local>
2017-12-02 21:48:30 +0000

    All the properties have been created for the textfields in the addViewController

--- MatrixCalculator/AddViewController.h
+++ MatrixCalculator/AddViewController.h
@@ -16,5 +16,77 @@
 
 - (IBAction)didselect1:(id)sender;
 
+//MatrixA Textfields
+@property (weak, nonatomic) IBOutlet UITextField *matrixA1;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA2;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA3;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA4;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA5;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA6;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA7;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA8;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA9;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA10;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA11;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA12;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA13;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA14;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA15;
+@property (weak, nonatomic) IBOutlet UITextField *matrixA16;
+
+//MatrixB Textfields
+@property (weak, nonatomic) IBOutlet UITextField *matrixB1;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB2;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB3;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB4;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB5;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB6;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB7;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB8;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB9;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB10;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB11;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB12;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB13;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB14;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB15;
+@property (weak, nonatomic) IBOutlet UITextField *matrixB16;
+
+//Result Textfields
+@property (weak, nonatomic) IBOutlet UITextField *result1;
+@property (weak, nonatomic) IBOutlet UITextField *result2;
+@property (weak, nonatomic) IBOutlet UITextField *result3;
+@property (weak, nonatomic) IBOutlet UITextField *result4;
+@property (weak, nonatomic) IBOutlet UITextField *result5;
+@property (weak, nonatomic) IBOutlet UITextField *result6;
+@property (weak, nonatomic) IBOutlet UITextField *result7;
+@property (weak, nonatomic) IBOutlet UITextField *result8;
+@property (weak, nonatomic) IBOutlet UITextField *result9;
+@property (weak, nonatomic) IBOutlet UITextField *result10;
+@property (weak, nonatomic) IBOutlet UITextField *result11;
+@property (weak, nonatomic) IBOutlet UITextField *result12;
+@property (weak, nonatomic) IBOutlet UITextField *result13;
+@property (weak, nonatomic) IBOutlet UITextField *result14;
+@property (weak, nonatomic) IBOutlet UITextField *result15;
+@property (weak, nonatomic) IBOutlet UITextField *result16;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @end
 
